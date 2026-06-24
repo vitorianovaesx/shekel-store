8aaad460e9453ef4c9806d7b2db92e0686e5a617
vitorianovaesx <vitoria.xavier@sou.inteli.edu.br>
2026-06-23 23:43:53 -0300

    video de explicacao

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('ShekelStore smoke test', (WidgetTester tester) async {
-    // Testes de integração requerem Supabase configurado.
-    // Execute o app diretamente com `flutter run` para testar.
-    expect(1 + 1, equals(2));
-  });
-}
